aa2fc98e7fc29aa74c5eb031ce5cfd5063d2d9c1
David Fallon <david.x.fallon@gsk.com>
2022-09-23 00:28:33 +0100

    issue2 added lemon drop powerup and lemon bullets effect.

--- game.py
+++ game.py
@@ -7,14 +7,15 @@ from math import sin, cos, radians
 pygame.init()
 
 # Set parent directory. 
-os.chdir("C:/Users/df717388/OneDrive - GSK/Documents/pygame/simpsons_space")
+os.chdir("C:/Users/df717388/OneDrive - GSK/Documents/pygame/simpsons_space_invaders")
 
 # Constants
 WIDTH, HEIGHT = 1600, 1000
 FPS = 60
 VEL = 6
 BULLET_VEL = 20
-BULLET_SIZE = 4
+DEFAULT_BULLET_DAMAGE = 1
+BULLET_SIZE = 8
 ANGLE_INCREMENT = 2
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
@@ -22,10 +23,20 @@ RED = (255, 0, 0)
 ENEMY_VEL = 1
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 50, 50
 START_TIME = time.time()
-ENEMY_FREQ = 3 # time in sec to add enemies to the screen. 
+ENEMY_FREQ = 2.5 # time in sec to add enemies to the screen. 
 ENEMY_WIDTH, ENEMY_HEIGHT = 50, 80
 ENEMY_CLASH_DAMAGE = 20
-SPLATTER_TIME = 2.0 # Time in sec to keep splattered enemies on screen. 
+SPLATTER_TIME = 2.0 # Time in sec to keep splattered enemies on screen.
+POWERUP_DROP_SPEED = 3 # Falling speed of powerups.  
+LEMON_DROP_FREQ = random.randint(20,40)
+POWERUP_TYPES = ["lemon"]
+LEMON_POWER_DUR = 10
+LEMON_POWERUP_VOL = 0.5
+LEMON_BULLET_DAMAGE = 3
+LEMON_BULLET_SIZE = 20
+BULLET_TYPES = ["default", "player", "lemon", "cola_bomb"]
+SMUG_FREQ = 7
+
 
 # Set up window
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -82,6 +93,7 @@ class Player(Character):
             pygame.image.load(os.path.join("assets", "health_icon.png")),
                                                 (self.health_icon_rect.width,
                                                  self.health_icon_rect.height)))
+        self.lemon_power_active = False
         
             
     def set_player_angle(self, keys_pressed) -> int:
@@ -139,47 +151,65 @@ class Player(Character):
         """Draw the health icon"""
         surface_to_blit.blit(self.health_icon, self.health_icon_rect)
         
+    def set_lemon_power(self, bool: bool):
+        """Turn on or off lemon power"""
+        self.lemon_power_active = bool
+        if self.lemon_power_active == True:
+            self.lemon_power_last_on_at = time.time()
+        
+        
 
 class Enemy(Character):
     """class for generic enemies"""
     def __init__(self, *args):
         Character.__init__(self, *args)
         
-    def move_towards(self, chase_x, chase_y, speed):
+    def move_towards(self, chase_x, chase_y):
         """Chase the player"""
         x_diff = chase_x - self.rect.centerx
         y_diff = chase_y - self.rect.centery
         if x_diff < -1:
-            self.rect.x -= speed # Move x by 1 and it's sign.
+            self.rect.x -= ENEMY_VEL # Move x by 1 and it's sign.
         elif x_diff > 1: 
-            self.rect.x += speed # Move x by 1 and it's sign.
+            self.rect.x += ENEMY_VEL # Move x by 1 and it's sign.
         else:
             self.rect.x = self.rect.x # Don't move if diff is zero. 
         if y_diff < -1:
-            self.rect.y -= speed
+            self.rect.y -= ENEMY_VEL
         elif y_diff > 1:
-            self.rect.y += speed
+            self.rect.y += ENEMY_VEL
         else:
             self.rect.y = self.rect.y
             
-    def kill_and_splat(self, new_image):
+    def kill_and_splat(self, new_image, rotate_randomly: bool):
         self.kill()
         self.change_image(new_image)
-        self.rotate_image_randomly()
+        if rotate_randomly:
+            self.rotate_image_randomly()
         self.mark_time()
+        
+    def set_hit_by(self, bullet_type):
+        """Record what bullet type last hit the enemy"""
+        if bullet_type in BULLET_TYPES:  
+            self.hit_by = bullet_type
+        else:
+            raise ValueError(f"{bullet_type} not allowed in list: {BULLET_TYPES}")
         
             
 class Bullet(pygame.sprite.Sprite):
     """Bullets can have an image, size, speed, direction, damage"""
-    def __init__(self, x_start, y_start, width, height, damage, angle_at_firing, image=None):
+    def __init__(self, x_start, y_start, width, height, damage, angle_at_firing, bullet_type, image) :
         pygame.sprite.Sprite.__init__(self)
         self.rect = pygame.Rect(x_start, y_start, width, height)
-        if image is not None:
-            self.image = pygame.transform.scale(
-                pygame.image.load(image),
-                (width, height))
+        self.image = pygame.transform.scale(
+            pygame.image.load(os.path.join("assets", image)),
+            (width, height))
         self.damage = damage
         self.angle_at_firing = angle_at_firing
+        if bullet_type in BULLET_TYPES:  
+            self.bullet_type = bullet_type
+        else:
+            raise ValueError(f"{bullet_type} not allowed in list: {BULLET_TYPES}")
         
     def move_bullet(self, bullet_speed):
         self.rect.x += sin(radians(self.angle_at_firing)) * bullet_speed
@@ -206,20 +236,53 @@ class Text:
         """Draw the text to a surface"""
         surface_to_blit.blit(self.text, self.rect)
         
+class Powerup(pygame.sprite.Sprite):
+    """Generic class for power-up items"""
+    def __init__(self, width, height, image, powerup_type):
+        pygame.sprite.Sprite.__init__(self)
+        self.rect = pygame.Rect(
+            random.randint(0,(WIDTH-width)), # x start
+            0 - height, # y start
+            width, height)
+        self.image = pygame.transform.scale(pygame.image.load(os.path.join("assets", image)), (width, height))
+        if powerup_type in POWERUP_TYPES:
+            self.powerup_type = powerup_type
+        else:
+            raise ValueError(f"'{powerup_type}' is not in allowed list: {POWERUP_TYPES}")
         
+    def move_downwards(self):
+        """Move the powerup down the screen"""
+        self.rect.y += POWERUP_DROP_SPEED
+        
+
 
 # Groups
 enemy_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
 enemies_killed_group = pygame.sprite.Group()
+powerup_group = pygame.sprite.Group()
+
+#----------
+# Functions
+#----------
+# Drop lemons
+time_last_lemon_drop_added = START_TIME # Initialise time zero
+def add_lemon_powerup(previous_time):
+    """Add a powerup to fall down screen"""
+    time_since_last_lemon_added = time.time() - previous_time
+    if time_since_last_lemon_added >= LEMON_DROP_FREQ:
+        new_lemon_drop = Powerup(40, 25, "lemon.png", "lemon")
+        powerup_group.add(new_lemon_drop)
+        global time_last_lemon_drop_added # Update time of last lemon drop
+        time_last_lemon_drop_added = time.time()
 
 # Release enemies
 time_last_enemy_added = START_TIME
-def add_enemy(frequency: float, previous_time) -> float:
+def add_enemy(previous_time):
     """Add an enemy to the screen given a time frequency in seconds"""
     time_since_last_enemy_added = time.time() - previous_time
     enemy_start_pos = random_enemy_start()
-    if time_since_last_enemy_added >= frequency:
+    if time_since_last_enemy_added >= ENEMY_FREQ:
         new_enemy = Enemy(
             os.path.join("assets", "kang_transparent.png"),
             enemy_start_pos[0],
@@ -231,13 +294,12 @@ def add_enemy(frequency: float, previous_time) -> float:
         global time_last_enemy_added # Update the time_last_enemy_added.  
         time_last_enemy_added = time.time()
     
-    
-# Functions
 def random_enemy_start():
     left_margin = (-ENEMY_WIDTH, random.randrange(0, HEIGHT*3/4))
     right_margin = (WIDTH, random.randrange(0, HEIGHT*3/4))
     top_margin = (random.randrange(0, (WIDTH - ENEMY_WIDTH)), -ENEMY_HEIGHT)
     return random.choice([left_margin, right_margin, top_margin])
+
 
 def play_sound(folder, volume, fadeout_ms=None, fadeout:bool=False):
     """Play random sound from folder"""
@@ -283,9 +345,20 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     player.get_degrees()
-                    bullet = Bullet(player.x_center,
-                                    player.y_center,
-                                    BULLET_SIZE, BULLET_SIZE, 1, player.player_deg)
+                    if player.lemon_power_active: # Lemon bullet
+                        bullet = Bullet(player.x_center,
+                                        player.y_center,
+                                        LEMON_BULLET_SIZE, LEMON_BULLET_SIZE,
+                                        LEMON_BULLET_DAMAGE, player.player_deg,
+                                        "lemon", "lemon.png"
+                                        )
+                    else: # normal bullet
+                        bullet = Bullet(player.x_center,
+                                        player.y_center,
+                                        BULLET_SIZE, BULLET_SIZE,
+                                        DEFAULT_BULLET_DAMAGE, player.player_deg,
+                                        "default", "default_bullet.png"
+                                        )
                     play_sound("shooting", 0.1, 200, True)
                     bullet_group.add(bullet)
                     
@@ -303,7 +376,7 @@ def main():
         
         # enemy movement
         for enemy in enemy_group:
-            enemy.move_towards(player.x_center, player.y_center, ENEMY_VEL)
+            enemy.move_towards(player.x_center, player.y_center)
         for bullet in bullet_group:
             bullet.move_bullet(BULLET_VEL)
             # Remove bullets that go off-screen. 
@@ -317,10 +390,12 @@ def main():
             for bullet in bullet_group:
                 if pygame.sprite.collide_rect(bullet, enemy):
                     bullet.kill() # Remove the bullet if it hit an enemy
-                    enemy.deplete_health(bullet.damage) # Deplete health of hit enemy.     
+                    enemy.deplete_health(bullet.damage) # Deplete health of hit enemy.
+                    enemy.set_hit_by(bullet.bullet_type) 
             # Check if enemies hit player
-            if pygame.sprite.collide_rect(player, enemy):
+            if pygame.sprite.collide_rect(enemy, player):
                 enemy.health = 0
+                enemy.set_hit_by("player")
                 player.deplete_health(ENEMY_CLASH_DAMAGE)
                 play_sound("player_hit", 0.4)
                 score -= 1
@@ -330,7 +405,12 @@ def main():
             elif enemy.health == 1:
                 enemy.change_image("kang_two_hits.png")
             elif enemy.health <= 0:
-                enemy.kill_and_splat("kang_splatted.png")
+                if enemy.hit_by == "lemon":
+                    enemy.kill_and_splat("kang_lemon_face.png", rotate_randomly=False)
+                elif enemy.hit_by in ["default", "player"]:
+                    enemy.kill_and_splat("kang_splatted.png", rotate_randomly=True)
+                else:
+                    raise ValueError(f"enemy.hit_by value '{enemy.hit_by}' not valid.") 
                 enemies_killed_group.add(enemy) # Transfer enemy to killed group.
                 play_sound("kang_kill", 0.3)
                 score += 1
@@ -340,34 +420,65 @@ def main():
         for enemy in enemies_killed_group:
             if (time.time() - enemy.timestamp) >= SPLATTER_TIME:
                 enemy.kill()
-         
+                
+        # powerup drop movement
+        for powerup in powerup_group:
+            powerup.move_downwards()
+            # remove powerups when they hit the bottom of the screen
+            if powerup.rect.bottom >= HEIGHT:
+                powerup.kill()
+            # Check if collected by player
+            if pygame.sprite.collide_rect(powerup, player):
+                play_sound("powerup", LEMON_POWERUP_VOL)
+                if powerup.powerup_type == "lemon":
+                    player.set_lemon_power(True)
+                    powerup.kill()
+        
+        # Turn off expired powerups
+        if player.lemon_power_active:
+            time_lemon_on = time.time() - player.lemon_power_last_on_at
+            lemon_time_remaining = round(LEMON_POWER_DUR - time_lemon_on)
+            lemon_text = Text("arial_bold", 30,
+                              f"Lemon power remaining: {lemon_time_remaining} sec",
+                              WHITE, 500, 940)
+            if (time_lemon_on) >= LEMON_POWER_DUR:
+                player.set_lemon_power(False)
+            
         # drawing section
         WIN.blit(SPACE, (0, 0)) # Fill the background window.
         score_text.update_text(f"Score: {score}", WHITE)
         score_text.draw_text(WIN)
         player_health_text.update_text(f"{player.health}%", WHITE)
         player_health_text.draw_text(WIN)
+        if player.lemon_power_active:
+            lemon_text.draw_text(WIN)
         player.draw_health_icon(WIN)
         WIN.blit(player.rotated_image, player.rect)
-        for bullet in bullet_group: # Can't use bullet_group.draw() as no image for bullets. 
-            bullet.draw_bullet(YELLOW)
-        add_enemy(ENEMY_FREQ, time_last_enemy_added)
+        bullet_group.draw(WIN)
+        add_enemy(time_last_enemy_added)
         enemy_group.draw(WIN)
+        add_lemon_powerup(time_last_lemon_drop_added)
+        powerup_group.draw(WIN)
         enemies_killed_group.draw(WIN)
         pygame.display.update()
         
         # Smug winning sound (ensures plays only once every 5 scores)
-        if score >= 5 and (score % 5 == 0) and (player.health > 0) and not played_smug_sound: # Every 5 scores. 
+        if score >= SMUG_FREQ and (score % SMUG_FREQ == 0) and (player.health > 0) and not played_smug_sound: # Every 5 scores. 
             play_sound("player_smug", 1.0)
             played_smug_sound = True
-        if score >= 5 and (score % 5 == 1): # Reset the boolean. 
+        if score >= SMUG_FREQ and (score % SMUG_FREQ == 1): # Reset the boolean. 
             played_smug_sound = False    
              
         # Update difficulty 
-        if score >= 20:
+        if score >= 10 and score < 20:
             ENEMY_FREQ = 2
-        elif score >= 40:
             ENEMY_VEL = 2
+        elif score >= 20 and score < 30:
+            ENEMY_FREQ = 1.5
+        elif score >= 45 and score < 60:
+            ENEMY_FREQ = 1
+        elif score >= 60:
+            ENEMY_FREQ, ENEMY_VEL = 1, 3
             
         # Check if player died
         if player.health <= 0:
